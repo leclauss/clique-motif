67aec0280a3571f6885b067ac5bb8e8b8c6713f6
leclauss <claussle@hu-berlin.de>
2021-01-19 16:54:10 +0100

    compile tsgenerator

--- compile.sh
+++ compile.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Compiles the sub-projects.
+# Needs the following packages to be installed: git make build-essential gcc g++ cmake pkg-config
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # update submodules
+echo "Updating submodules ..."
 git submodule update --init --recursive
 
 # algorithms
@@ -12,6 +16,7 @@ cd $DIR/algorithms
 cd clique
 
 # lmc
+echo "Compiling LMC ..."
 cd lmc
 make all
 cd ..
@@ -23,6 +28,7 @@ cd ..
 cd graph
 
 # scamp
+echo "Compiling SCAMP ..."
 cd scamp
 mkdir build
 cd build
@@ -31,5 +37,24 @@ cmake --build . --config Release
 cd ../..
 
 # end of graph algorithms
-cd ..
+
+# tsgenerator
+echo "Compiling tsgenerator-dev ..."
+cd $DIR/tsgenerator
+cd tsgenerator-dev
+mkdir build
+cd build
+cmake ..
+make
+echo "Creating tsgenerator-dev package ..."
+make package
+echo "Installing tsgenerator-dev ..."
+sudo apt install ./tsgenerator-dev-3.0.0.deb
+sudo ldconfig
+echo "Compiling tsgenerator ..."
+cd ../../tsgenerator
+mkdir build
+cd build
+cmake ..
+make
 
